aea479bc25733e107fcd991490f0802c89e6a975
skippor <125205616+skippor@users.noreply.github.com>
2023-08-18 08:14:35 +0000

    add: xproxy

---
+++ study/xproxy/src/lib.rs
@@ -0,0 +1,38 @@
+use std::error::Error;
+
+pub struct Config {
+    cfg: String
+}
+
+impl Config {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        // skip the first param
+        args.next();
+
+        let cfg = match args.next() {
+            Some(arg) => arg,
+            None => return Err("need a config file")
+        };
+
+        Ok(Config{
+            cfg
+        })
+    }
+}
+
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    println!("run config is {}", config.cfg);
+    Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn must_eq() {
+        assert_eq!(2, 1 + 1);
+    }
+}
---
+++ study/xproxy/src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::process;
+
+use xproxy::Config;
+
+fn main() {
+    let only_test = env::var("ONLY_TEST").is_ok();
+    if only_test {
+        println!("only a test!");
+        process::exit(0)
+    }
+
+
+    let cfg = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(-1);
+    });
+
+    if let Err(e) = xproxy::run(cfg) {
+        eprintln!("Application error: {e}");
+        process::exit(-1);
+    }
+}
